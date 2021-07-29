0bf84458a048c8a3bc692eed84187ba623648298
Alexandru Barbur <alex@ctrlc.name>
2021-07-29 12:09:39 -0400

    Implement dictdb signal handling.

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
   * https://gcc.gnu.org/projects/cxx-status.html
   * https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html
   * https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html
+* C++ Style
+  * [Typedef Types](https://stackoverflow.com/a/612476/937006)
--- pkg/dictdb/main.cpp
+++ pkg/dictdb/main.cpp
@@ -1,4 +1,68 @@
+// C++ standard library
+#include <string>
+#include <iostream>
+#include <unordered_set>
+
+// C standard library
+#include <signal.h>     // sigaction, signal
+
+
+// Words database.
+typedef struct {
+  // https://en.cppreference.com/w/cpp/container/unordered_set
+  std::unordered_set<std::string> words;
+} dictdb_t;
+
+// Flag to indicate if the process should continue running.
+bool running = false;
+
+// Process signals.
+void handle_signal(int sig) {
+  switch (sig) {
+    // sent by process manager to indicate we should reload
+    case SIGHUP:
+      // XXX: reload daemon configuration files and reopen file descriptors
+      break;
+
+    case SIGTERM:
+    case SIGINT:
+      running = false;
+      break;
+  }
+}
+
 // Application entry point.
 int main(int argc, char* argv[]) {
-  return 0;
+  // register signal handler for relevant signals
+  struct sigaction sa;
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  sa.sa_handler = handle_signal;
+
+  if (sigaction(SIGHUP, &sa, NULL) < 0) {
+    std::cerr << "failed to install SIGHUP signal handler" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  if (sigaction(SIGTERM, &sa, NULL) < 0) {
+    std::cerr << "failed to install SIGTERM signal handler"  << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  if (sigaction(SIGINT, &sa, NULL) < 0) {
+    std::cerr << "failed to install SIGINT signal handler"  << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  // initialize the database
+  dictdb_t db;
+
+  // process requests
+  running = true;
+  while (running) {
+    // TODO
+  }
+
+  // successful exit
+  exit(EXIT_SUCCESS);
 }
