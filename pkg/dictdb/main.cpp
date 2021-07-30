5891da960a17eaaac4225bccd39fdcccabee3189
Alexandru Barbur <alex@ctrlc.name>
2021-07-30 07:58:57 -0400

    Minor refactoring.

--- pkg/dictdb/main.cpp
+++ pkg/dictdb/main.cpp
@@ -6,36 +6,9 @@
 // C standard library
 #include <signal.h>     // sigaction, signal
 
-// Intel TBB
-#include <oneapi/tbb/concurrent_hash_map.h>
+// DictDB
+#include "data.h"
 
-// XXX
-// https://spec.oneapi.io/versions/latest/elements/oneTBB/source/containers/concurrent_hash_map_cls.html
-typedef tbb::concurrent_hash_map<std::string, bool> dictdb_word_map_t;
-
-// XXX
-typedef struct {
-  dictdb_word_map_t words;
-} dictdb_t;
-
-// XXX
-typedef enum struct OperationType:uint8_t {
-  PING = 0,
-  INSERT = 1,
-  SEARCH = 2,
-  DELETE = 3
-} dictdb_op_type_t;
-
-// XXX
-typedef struct {
-  dictdb_op_type_t type;
-  std::string data;
-} dictdb_op_request_t;
-
-// XXX
-typedef struct {
-  uint8_t result;
-} dictdb_op_response_t;
 
 // Flag to indicate if the process should continue running.
 bool running = false;
